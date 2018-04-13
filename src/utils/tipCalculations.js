d6d425e082b8e04482e010a22acda85b807cc5b6
Emily Howard <emilyhoward@Bruces-MacBook-Pro.local>
2018-04-12 21:35:14 -0400

    Started creating all the tip functions: getHighestTipDay, getHighestHourlyDay, getTotalTips, getTotalHours, dayFilter, getDayOfWeekData, getBestTipDayOfWeek, getBestHourlyDayOfWeek and getStatistics

---
+++ src/utils/tipCalculations.js
@@ -0,0 +1,48 @@
+//Takes an array, returns the day object with the highest tipAmount
+const getHighestTipDay = arr => {
+
+};
+
+//Takes an array, returns the day object with the highest hourly amount
+const getHighestHourlyDay = arr => {
+
+
+};
+
+//Sums and returns the total tipAmount from a given aray
+const getTotalTips = arr => {
+
+};
+
+//Sums and returns the total hours from a given array
+const getTotalHours = arr => {
+
+};
+
+//Filters a given array by dayName, used to gather stats in the following getDayOfWeekData function
+const dayFilter = (arr, dayName) => {
+
+};
+
+//Loops through the data of a given array, and filters the data by dayName, returning an object with properites based
+//on the previous functions.
+const getDayOfWeekData = arr => {
+
+};
+
+//Operates on the dayOfWeekData object returned by the getDayOfWeekData function.  Returns a day of the week with the highest gross tipAmount
+const getBestTipDayOfWeek = obj => {
+
+};
+
+//Operates on the dayOfWeekData object returned by the getDayOfWeekData function.  Returns a day of the week with the highest hourlyAverage
+const getBestHourlyDayOfWeek = obj => {
+
+};
+
+//This function uses all previous functions to generate an object to be used on the Summary page with all neccessary data
+const getStatistics = arr => {
+
+};
+
+export default getStatistics;
