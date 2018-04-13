3b4d9724e069010a111fa88d4f50a35f2e556ca9
Joe Vance <joevance@local.local>
2018-04-12 22:32:18 -0400

    updated the index.js in the src folder

--- src/index.js
+++ src/index.js
@@ -3,7 +3,8 @@ import ReactDOM from 'react-dom';
 import injectTapEventPlugin from 'react-tap-event-plugin';
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 import { BrowserRouter } from 'react-router-dom';
-
+import AppContainer from './AppContainer';
+import registerServiceWorker from './registerServiceWorker';
 import {
   ApolloClient,
   createNetworkInterface,
@@ -14,6 +15,10 @@ import {
   addGraphQLSubscriptions
 } from 'subscriptions-transport-ws';
 import { GC_AUTH_TOKEN } from './utils/constants';
+
+const networkInterface = createNetworkInterface({
+  uri: 'https://api.graph.cool/simple/v1/cj62iufbl5dtj0150l36xa1j4'
+});
 
 networkInterface.use([
   {
@@ -27,3 +32,31 @@ networkInterface.use([
     }
   }
 ]);
+// Create WebSocket client
+
+const wsClient = new SubscriptionClient(
+  'wss://subscriptions.us-west-2.graph.cool/v1/cj62iufbl5dtj0150l36xa1j4',
+  {
+    reconnect: true
+  }
+);
+const networkInterfaceWithSubscriptions = addGraphQLSubscriptions(
+  networkInterface,
+  wsClient
+);
+const apolloClient = new ApolloClient({
+  networkInterface: networkInterfaceWithSubscriptions
+});
+
+injectTapEventPlugin();
+ReactDOM.render(
+  <ApolloProvider client={apolloClient}>
+    <BrowserRouter>
+      <MuiThemeProvider>
+        <AppContainer />
+      </MuiThemeProvider>
+    </BrowserRouter>
+  </ApolloProvider>,
+  document.getElementById('root')
+);
+registerServiceWorker();
