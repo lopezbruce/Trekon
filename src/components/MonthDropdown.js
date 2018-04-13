70c23fa8953a366b26077fcff74865472f7f1310
Lorena Perez <lorena@local.local>
2018-04-13 12:20:14 -0400

    Created the MonthDropdown

---
+++ src/components/MonthDropdown.js
@@ -0,0 +1,36 @@
+import React, { Component } from 'react';
+import { MenuItem, DropDownMenu } from 'material-ui';
+
+const monthNames = [
+  'January',
+  'February',
+  'March',
+  'April',
+  'May',
+  'June',
+  'July',
+  'August',
+  'September',
+  'October',
+  'November',
+  'December'
+];
+const months = monthNames.map((month, index) => (
+  <MenuItem value={index} key={index} primaryText={monthNames[index]} />
+));
+
+export default class MonthDropdown extends Component {
+  render() {
+    return (
+      <DropDownMenu
+        maxHeight={300}
+        value={this.props.value}
+        onChange={this.props.handleMonthChange}
+        autoWidth={false}
+        style={styles}
+      >
+        {months}
+      </DropDownMenu>
+    );
+  }
+}
