9e3e1573d193622ade8aea4147124847e7f9809a
Mo Diallo <modiallo@local.local>
2018-04-13 12:57:38 -0400

    Updated Summary component

--- src/components/Summary.js
+++ src/components/Summary.js
@@ -1,20 +1,78 @@
 import React, { Component } from 'react';
+import styled from 'styled-components';
+import CalendarHeader from './CalendarHeader';
+import getStatistics from '../utils/tipCalculations';
+import suffixer from '../utils/suffixer';
+import ScrollAnimation from 'react-animate-on-scroll';
 
 class Summary extends Component {
   constructor(props) {
     super(props);
+    let date = new Date();
+    this.state = {
+      year: date.getFullYear(),
+      month: date.getMonth()
     };
   }
 
-  //handleNext = () => { };
+  handleNext = () => {
+    if (this.state.month === 11) {
+      this.setState(prevState => ({
+        month: 0,
+        year: prevState.year + 1
+      }));
+    } else {
+      this.setState(prevState => ({
+        month: prevState.month + 1
+      }));
+    }
+  };
 
-  //handlePrev = () => { };
+  handlePrev = () => {
+    if (this.state.month === 0) {
+      this.setState(prevState => ({
+        month: 11,
+        year: prevState.year - 1
+      }));
+    } else {
+      this.setState(prevState => ({
+        month: prevState.month - 1
+      }));
+    }
+  };
 
   render() {
     //Here we filter our data to only diplay from the month selected in the CalendarHeader Component
     //Data comes from various helper functions in tipSummaryCalculations in the utils folder
-
-    return <div className={this.props.className}>console.log('test')</div>;
+    const filteredTipsByMonth = this.props.data.User.tips.filter(
+      data => data.month === this.state.month && data.year === this.state.year
+    );
+    const summaryData = getStatistics(filteredTipsByMonth);
+    const monthNames = [
+      'January',
+      'February',
+      'March',
+      'April',
+      'May',
+      'June',
+      'July',
+      'August',
+      'September',
+      'October',
+      'November',
+      'December'
+    ];
+    return (
+      <div className={this.props.className}>
+        <CalendarHeader
+          handleNext={this.handleNext}
+          handlePrev={this.handlePrev}
+          year={this.state.year}
+          month={this.state.month}
+        />
+      </div>
+    );
   }
+}
 
-export default Summary;
+export default styled(Summary);
