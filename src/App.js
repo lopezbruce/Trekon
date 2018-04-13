ed439cfa67bae0fff73c2efe5acbb7ab3781c2ec
Joe Vance <joevance@local.local>
2018-04-12 22:19:23 -0400

    update the Apps.js to inculde the Nav Calender Summary Statitics Welcome and created a switch and Route the user to the correct component path

--- src/App.js
+++ src/App.js
@@ -2,25 +2,68 @@ import React from 'react';
 import styled from 'styled-components';
 import { Route, Switch, withRouter } from 'react-router-dom';
 import { graphql, gql } from 'react-apollo';
+import Nav from './components/Nav';
+import Calendar from './components/Calendar';
+import Summary from './components/Summary';
+import Statistics from './components/Statistics';
+import Welcome from './components/Welcome';
 
 class App extends React.Component {
   componentDidMount() {
-    /*
+
     //TODO: Set up functional user subscriptions to get rid of page reloading on data changes
-    this.props.subscribeToUserChange({
-      id: this.props.id,
-    })
-    */
+
   }
 
   render() {
-    return <div className={this.props.className}>Test</div>;
+    if (this.props.userQuery.loading === true) {
+      return <div>LOADING...</div>;
+    }
+
+    return (
+      <div className={this.props.className}>
+        <Nav data={this.props.userQuery} />
+        <Switch>
+          <Route
+            path="/Calendar"
+            component={() => (
+              <Calendar
+                data={this.props.userQuery}
+                loading={this.props.userQuery.loading}
+              />
+            )}
+          />
+          <Route
+            path="/Summary"
+            component={() => <Summary data={this.props.userQuery} />}
+            loading={this.props.userQuery.loading}
+          />
+          <Route
+            path="/Statistics"
+            component={() => (
+              <Statistics
+                data={this.props.userQuery}
+                loading={this.props.userQuery.loading}
+              />
+            )}
+          />
+          <Route path="/*" component={Welcome} />
+        </Switch>
+      </div>
+    );
   }
 }
-/*
-//TODO: Set up functional user subscriptions to get rid of page reloading on data changes
 
 //TODO: Set up functional user subscriptions to get rid of page reloading on data changes
 
-export default AppWithData(withRouter(StyledApp));
-*/
+
+const AppWithData = graphql(userQuery, {
+  name: 'userQuery',
+  options: ownProps => ({ variables: { id: ownProps.id } })
+
+//TODO: Set up functional user subscriptions to get rid of page reloading on data changes
+
+});
+
+
+export default AppWithData(withRouter());
